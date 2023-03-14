2f23c01913af679f788041fc63e4a7d7d8cf31ab
zhaoshuhao <zhaoshuhao@qianxin.com>
2023-03-14 14:54:39 +0800

    实现一个对象池 但是在释放是存在问题

--- object_pool/main.cpp
+++ object_pool/main.cpp
@@ -1,6 +1,72 @@
+#include "my_object_pool.h"
 #include <iostream>
-#include "object_pool.h"
+using namespace std;
+
+class Test {
+public:
+    Test() = default;
+    Test(int a, int b) {}
+    Test(int a, double b) {}
+    void show(int num) {
+        cout << num << endl;
+    }
+};
+class BigObject {
+public:
+    BigObject() {}
+
+    BigObject(int a) {}
+
+    BigObject(const int &a, const int &b) {
+    }
+    ~BigObject(){ cout << "disconstruct BigObject" << endl;}
+
+    void Print(const string &str) {
+        cout << str << endl;
+    }
+};
+
+void Print(const shared_ptr<BigObject>& p, const string &str) {
+    if (p != nullptr) {
+        p->Print(str);
+        return;
+    }
+    cout << "this shared_ptr is null str: " << str <<endl;
+
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ObjectPool<BigObject> pool;
+    pool.init(2);
+
+    {
+        auto p = pool.get();
+        Print(p, "p");
+
+        auto p2 = pool.get();
+        Print(p2, "p2");
+        cout << "pool size: "<< pool.size() <<endl;
+    }
+
+    cout << "------------------" << endl;
+    cout << "pool size: "<< pool.size() <<endl;
+    auto p = pool.get();
+    Print(p, "p");
+
+    auto p2 = pool.get();
+    Print(p2, "p2");
+
+    auto p3 = pool.get();
+    Print(p3, "p3");
+
+    cout << "------------------" << endl;
+    cout << "pool size: "<< pool.size() <<endl;
+
+    pool.init(2, 1);
+
+    cout << "pool size: "<< pool.size() <<endl;
+
+    auto p4 = pool.get<int>();
+    cout << "pool size: "<< pool.size() <<endl;
+    Print(p4, "p4");
     return 0;
 }
---
+++ object_pool/my_object_pool.h
@@ -0,0 +1,63 @@
+//
+// Created by zhaoshuhao on 2023/3/14.
+//
+
+#ifndef OBJECT_POOL_MY_OBJECT_POOL_H
+#define OBJECT_POOL_MY_OBJECT_POOL_H
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+class NoCopyable {
+public:
+    NoCopyable() = default;
+    virtual ~NoCopyable() = default;
+    NoCopyable(const NoCopyable &) = delete;
+    NoCopyable &operator=(const NoCopyable &) = delete;
+};
+
+const size_t MaxObjectNum = 10;
+template<typename T>
+class ObjectPool : public NoCopyable {
+public:
+    template<typename... Args>
+    using construct = std::function<std::shared_ptr<T>(Args...)>;
+
+    template<typename... Args>
+    void init(size_t num, Args... args) {
+        if (num <= 0 || num > MaxObjectNum)
+            throw std::logic_error("object pool num is error");
+
+        std::string construct_name = typeid(construct<Args...>).name();
+        std::cout << "construct_name " << construct_name << std::endl;
+
+        for (size_t i = 0; i < num; i++) {
+            object_maps_.emplace(construct_name, std::shared_ptr<T>(new T(std::forward<Args>(args)...), [construct_name, this](T *p) {
+                                     object_maps_.emplace(std::move(construct_name), std::shared_ptr<T>(p));
+                                 }));
+        }
+    }
+
+    template<typename... Args>
+    std::shared_ptr<T> get() {
+        std::string construct_name = typeid(construct<Args...>).name();
+        auto range = object_maps_.equal_range(construct_name);
+        for (auto it = range.first; it != range.second; it++) {
+            auto ptr = it->second;
+            object_maps_.erase(it);
+            return ptr;
+        }
+        return nullptr;
+    }
+
+    size_t size() const{
+        return object_maps_.size();
+    }
+
+private:
+    std::multimap<std::string, std::shared_ptr<T>> object_maps_;
+};
+#endif//OBJECT_POOL_MY_OBJECT_POOL_H
