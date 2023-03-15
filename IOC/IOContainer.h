abed720d7ca1b6edb831626c47fc215dd102fac5
kris hao <76926060+kris-zsh@users.noreply.github.com>
2023-03-15 22:49:37 +0800

    写出register_type特化版本 只支持一个参数，这样写看起来更好一些

--- IOC/IOContainer.h
+++ IOC/IOContainer.h
@@ -5,59 +5,66 @@
 #ifndef IOC_IOCONTAINER_H
 #define IOC_IOCONTAINER_H
 
-#include <map>
-#include <functional>
-#include <string>
-#include <stdexcept>
-#include <memory>
 #include <cassert>
+#include <functional>
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <string>
 
 class NoCopyable {
 public:
     NoCopyable() = default;
     virtual ~NoCopyable() = default;
-    NoCopyable(const NoCopyable&) = delete;
-    NoCopyable& operator=(const NoCopyable&) = delete;
+    NoCopyable(const NoCopyable &) = delete;
+    NoCopyable &operator=(const NoCopyable &) = delete;
 };
 
-class IOContainer : public NoCopyable{
+class IOContainer : public NoCopyable {
 public:
-    IOContainer() =default;
+    IOContainer() = default;
     ~IOContainer() override = default;
 
     template<typename T, typename Depend = void>
-    void register_type(const std::string& key){
-        using create_object = std::function<T*()>;
-        create_object ob = []{
-            if constexpr (std::is_same_v<Depend,void>)
-                return new T();
-            else
-                return new T(new Depend());
+    void register_type(const std::string &key) {
+        using create_object = std::function<T *()>;
+        create_object ob = [] {
+            return new T(new Depend());
         };
-        register_type(key,ob);
+        register_type(key, ob);
     }
+
     template<typename T>
-    T* resolve_ptr(const std::string& key){
-        using create_object = std::function<T*()>;
+    void register_type(const std::string &key) {
+        using create_object = std::function<T *()>;
+        create_object ob = [] {
+            return new T();
+        };
+        register_type(key, ob);
+    }
+
+    template<typename T>
+    T *resolve_ptr(const std::string &key) {
+        using create_object = std::function<T *()>;
         auto it = create_maps.find(key);
         auto fun = std::any_cast<create_object>(it->second);
         return fun();
     }
 
     template<typename T>
-    std::shared_ptr<T> resolve_smart_ptr(const std::string& key){
-        T* t = resolve_ptr<T>(key);
+    std::shared_ptr<T> resolve_smart_ptr(const std::string &key) {
+        T *t = resolve_ptr<T>(key);
         return std::shared_ptr<T>(t);
     }
 
 private:
-    void register_type(const std::string& key, std::any any){
-        if(create_maps.find(key) != create_maps.end())
+    void register_type(const std::string &key, std::any any) {
+        if (create_maps.find(key) != create_maps.end())
             throw std::invalid_argument("this key already exist");
         create_maps[key] = any;
     }
 
-    std::map<std::string,std::any >create_maps;
+    std::map<std::string, std::any> create_maps;
 };
 
 #endif//IOC_IOCONTAINER_H
